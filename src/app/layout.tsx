9c317d7359655b88e321224565937f089a99130a
Gonzalo Parra <gonzzaparra@gmail.com>
2025-07-18 12:25:46 -0300

    feat: add vercel and google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,25 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 
-import Header from '@/components/common/header';
-import Footer from '@/components/common/footer';
-import { ThemeProvider } from '@/components/theme-provider';
-import { Toaster } from '@/components/ui/sonner';
+import Header from "@/components/common/header";
+import Footer from "@/components/common/footer";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
-import { cn } from '@/lib/utils';
-import { metadata as metadataConstants } from '@/constants/metadata';
-import './globals.css';
+import { cn } from "@/lib/utils";
+import { metadata as metadataConstants } from "@/constants/metadata";
+
+import Analytics from "./analytics";
+import "./globals.css";
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -30,22 +32,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html suppressHydrationWarning lang='es'>
+    <html suppressHydrationWarning lang="es">
       <body
         className={cn(
           `${geistSans.variable} ${geistMono.variable} antialiased`,
-          'min-h-screen antialiased',
-          'dark:bg-black/[0.96] bg-zinc-100',
+          "min-h-screen antialiased",
+          "dark:bg-black/[0.96] bg-zinc-100",
         )}
       >
-        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-          <div className='flex flex-col min-h-screen text-foreground'>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <div className="flex flex-col min-h-screen text-foreground">
             <Header />
-            <main className='flex-1 w-full'>{children}</main>
+            <main className="flex-1 w-full">{children}</main>
             <Footer />
           </div>
           <Toaster />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
