56bc5b3266476aed376974706174319f25d25501
JaviCeRodriguez <javicerodriguez@gmail.com>
2025-07-21 17:07:29 -0300

    feat: use generateStaticParams to generate statically all community pages

--- src/app/community/[slug]/page.tsx
+++ src/app/community/[slug]/page.tsx
@@ -1,17 +1,27 @@
-import { fetchCommunityBySlug } from '@/lib/fetch-communities';
-import CommunityContent from './community-content';
+import {
+  fetchCommunities,
+  fetchCommunityBySlug,
+} from "@/lib/fetch-communities";
+import CommunityContent from "./community-content";
+
+export const generateStaticParams = async () => {
+  const communities = fetchCommunities();
+  return communities.map((community) => ({
+    slug: community.slug,
+  }));
+};
 
 export default async function CommunityPage({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) {
-  const { slug } = await params;  
-  const community = await fetchCommunityBySlug(slug);
+  const { slug } = await params;
+  const community = fetchCommunityBySlug(slug);
   if (!community) {
     return (
-      <div className='container mx-auto px-4 py-8'>
-        <div className='text-2xl font-bold text-red-500'>
+      <div className="container mx-auto px-4 py-8">
+        <div className="text-2xl font-bold text-red-500">
           Community not found
         </div>
       </div>
@@ -19,7 +29,7 @@ export default async function CommunityPage({
   }
 
   return (
-    <div className='min-h-screen w-full'>
+    <div className="min-h-screen w-full">
       <CommunityContent community={community} />
     </div>
   );
